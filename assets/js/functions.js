98b040bd5d21871df79485972b7cc693f4460cd8
Akash <ak.chourasia.007@gmail.com>
2020-08-20 10:54:51 +0530

    commit

---
+++ assets/js/functions.js
@@ -0,0 +1,51 @@
+var xhttp;
+xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        var resultData = "";
+        var top = 0;
+        var totalMarks;
+        data.sort(GetSortOrder("name"));
+        for (var i = 0; i < data.length; i++) {
+
+            var tag = "pass";
+            totalMarks = parseInt(data[i].marks.Maths) + parseInt(data[i].marks.English) + parseInt(data[i].marks.Science);
+            if (top < totalMarks) {
+                top = totalMarks;
+            }
+        }
+        for (var i = 0; i < data.length; i++) {
+            var total;
+            var tag = "pass";
+            for (var j in data[i].marks) {
+                if (parseInt(data[i].marks.Maths) < 20 || parseInt(data[i].marks.English) < 20 || parseInt(data[i].marks.Science) < 20) {
+                    tag = "fail"
+                }
+                total = parseInt(data[i].marks.Maths) + parseInt(data[i].marks.English) + parseInt(data[i].marks.Science);
+            }
+            if (tag == "fail") {
+                resultData = resultData + "<tr style=\"color:red;\" ><td >" + data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1) + "</td><td>" + data[i].rollNumber + "</td><td>" + total + "</td><td>Fail</td></tr>";
+
+            } else if (top == total) {
+                resultData = resultData + "<tr style=\"color:green;\" ><td >" + data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1) + "</td><td>" + data[i].rollNumber + "</td><td>" + total + "</td><td>Topper</td></tr>";
+            } else {
+                resultData = resultData + "<tr ><td >" + data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1) + "</td><td>" + data[i].rollNumber + "</td><td>" + total + "</td><td>Pass</td></tr>";
+            }
+        }
+        document.getElementById("result").innerHTML = resultData;
+    }
+};
+xhttp.open("GET", "http://localhost/dummy/getResult.json", true);
+xhttp.send();
+
+function GetSortOrder(prop) {
+    return function(a, b) {
+        if (a[prop] > b[prop]) {
+            return 1;
+        } else if (a[prop] < b[prop]) {
+            return -1;
+        }
+        return 0;
+    }
+}
